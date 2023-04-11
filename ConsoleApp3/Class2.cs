1029fb864b530e283e92135920221f105f97f95c
The4yo <eugshpak@yandex.ru>
2023-04-11 22:39:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Auto : ITS
+    {
+        public void Move()
+        {
+            Console.WriteLine("Авто едет");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp3;
+using System.Diagnostics;
+using System.Numerics;
+
+Auto auto = new Auto();
+Train train = new Train();
+Plane1 plane = new Plane1();
+auto.Move();
+train.Move();
+plane.Move();
